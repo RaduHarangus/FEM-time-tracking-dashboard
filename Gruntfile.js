613013a6028425dc8926ad58c56c27561b61247f
Radu H <52208792+Radoogle@users.noreply.github.com>
2022-01-10 20:50:55 +0200

    progress save

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        // concat: {
+        //     main: {
+        //         src: [
+        //             'js/nav.js',
+        //             'js/subscribe.js',
+        //             'js/timer.js',
+        //             'js/carousel.js',
+        //             'js/contact.js',
+        //             'js/tooltips.js',
+        //             'js/forms.js',
+        //             'js/menu.js',
+        //             'js/modal.js',
+        //             'js/tabs.js',
+        //             'js/emailValidation.js',
+        //             'js/scrollTop.js',
+        //         ],
+        //         dest: 'main.js',
+        //         options: {
+        //             separator: '\n\n',
+        //             interrupt: true
+        //         }
+        //     }
+        // },
+        less: {
+            files: {
+                src: [
+                    'less/main.less',
+                ],
+                dest: 'style.css'
+            }
+        },
+        watch: {
+            less: {
+                files: [
+                    'less/*.less',
+                    'less/**/*.less'
+                ],
+                tasks: ['less']
+            }
+            // ,
+            // js: {
+            //     files: [
+            //         'js/*.js'
+            //     ],
+            //     tasks: ['concat']
+            // }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // the default task can be run just by typing "grunt" on the command line
+    grunt.registerTask('default', ['less', 'watch']);
+}
